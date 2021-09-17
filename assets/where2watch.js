b211121f1042e5c983038a9caac945ef4e681f0d
Derick Hooley <dghooley@gmail.com>
2021-09-16 19:19:52 -0500

    updatedAPI

--- assets/where2watch.js
+++ assets/where2watch.js
@@ -1,17 +1,15 @@
-var axios = require("axios").default;
-
-var options = {
-  method: 'GET',
-  url: 'https://imdb8.p.rapidapi.com/auto-complete',
-  params: {q: 'game of thr'},
-  headers: {
-    'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-    'x-rapidapi-key': 'a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+// IMDB API ===============>
+fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "imdb8.p.rapidapi.com",
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
+// =========================>
