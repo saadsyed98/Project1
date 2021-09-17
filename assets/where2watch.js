82db16172f5cc49514ec3e3d38640e2e454eebcc
Derick Hooley <dghooley@gmail.com>
2021-09-16 19:15:07 -0500

    bug fix

--- assets/where2watch.js
+++ assets/where2watch.js
@@ -1,22 +1,13 @@
-$(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
-    let searchText = $('#searchText').val();
-    getMovies(searchText);
-    e.preventDefault();
-});
-});
-
 var axios = require("axios").default;
 
 var options = {
-  method: 'POST',
-  url: 'https://gowatch.p.rapidapi.com/lookup/title/imdb_id',
+  method: 'GET',
+  url: 'https://imdb8.p.rapidapi.com/auto-complete',
+  params: {q: 'game of thr'},
   headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'x-rapidapi-host': 'gowatch.p.rapidapi.com',
+    'x-rapidapi-host': 'imdb8.p.rapidapi.com',
     'x-rapidapi-key': 'a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5'
-  },
-  data: {id: 'tt6751668', type: 'movie', country: 'us'}
+  }
 };
 
 axios.request(options).then(function (response) {
