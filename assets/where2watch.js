7efd96e55eae334ce187547c18631fac93e09ef6
Derick Hooley <dghooley@gmail.com>
2021-09-16 22:04:43 -0500

    activeAPI's

--- assets/where2watch.js
+++ assets/where2watch.js
@@ -1,8 +1,9 @@
-// IMDB API ===============>
-fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
+
+// Utelly API =========================>
+fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt3398228&source=imdb&country=us", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "imdb8.p.rapidapi.com",
+		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
 		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
 	}
 })
@@ -12,4 +13,27 @@ fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
 .catch(err => {
 	console.error(err);
 });
-// =========================>
+// ========================>
+
+
+// GoWATCH ========================>
+fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-host": "gowatch.p.rapidapi.com",
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
+	},
+	"body": {
+		"id": "tt6751668",
+		"type": "movie",
+		"country": "us"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+// ==============================>
