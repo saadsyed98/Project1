5325a8061d1a790eaabbcbbccb498fc3cfdd5373
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-09-16 20:08:33 -0400

    goWatch fetch statement

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// goWatch fetch
+fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-host": "gowatch.p.rapidapi.com",
+		"x-rapidapi-key": "d467e557fbmsha81ae590ccc3b5cp1b37e8jsn67b4a6f59821"
+	},
+	"body": {
+		"id": "tt6751668",
+		"type": "movie",
+		"country": "us"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
