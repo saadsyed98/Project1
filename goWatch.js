c9189593381ddf4b9db0fb3cffc835fabf8dd76f
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-09-19 10:47:34 -0400

    lance js

--- goWatch.js
+++ goWatch.js
@@ -1,20 +1,75 @@
-// fetching the user's input front the search box
-var title = $(".search-field").val()
-$(document).ready(function() {
-    $(".search-btn").click(function(){
-        title;
-    });
-});
+// // fetching the user's input front the search box
+// var title = $(".search-field").val()
+// $(document).ready(function() {
+//     $(".search-btn").click(function(){
+//         title;
+//     });
+// });
 
-fetch("https://gowatch.p.rapidapi.com/lookup/title/tmdb_id?id=496243&type=movie&country=us" + title, {
+// fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title=" + title, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+// 		"x-rapidapi-key": "d467e557fbmsha81ae590ccc3b5cp1b37e8jsn67b4a6f59821"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+// fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
+// 	"method": "POST",
+// 	"headers": {
+// 		"content-type": "application/x-www-form-urlencoded",
+// 		"x-rapidapi-host": "gowatch.p.rapidapi.com",
+// 		"x-rapidapi-key": "d467e557fbmsha81ae590ccc3b5cp1b37e8jsn67b4a6f59821"
+// 	},
+// 	"body": {
+// 		"id": "tt6751668",
+// 		"type": "movie",
+// 		"country": "us"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+
+
+
+// Utelly API =========================>
+fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt3398228&source=imdb&country=us", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+// ========================>
+
+
+// GoWATCH ========================>
+fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
 	"method": "POST",
 	"headers": {
 		"content-type": "application/x-www-form-urlencoded",
 		"x-rapidapi-host": "gowatch.p.rapidapi.com",
-		"x-rapidapi-key": "d467e557fbmsha81ae590ccc3b5cp1b37e8jsn67b4a6f59821"
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
 	},
 	"body": {
-		"id": "496243",
+		"id": "tt6751668",
 		"type": "movie",
 		"country": "us"
 	}
@@ -25,3 +80,4 @@ fetch("https://gowatch.p.rapidapi.com/lookup/title/tmdb_id?id=496243&type=movie&
 .catch(err => {
 	console.error(err);
 });
+// ==============================>
