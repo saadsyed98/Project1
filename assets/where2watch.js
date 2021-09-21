a066abc3359c598c61739f051448ab32c17cbfc2
Derick Hooley <dghooley@gmail.com>
2021-09-20 23:09:35 -0500

    javaUpdates

--- assets/where2watch.js
+++ assets/where2watch.js
@@ -11,7 +11,7 @@ function getMovies(searchText) {
   axios.get('http://www.omdbapi.com?s='+ searchText+'&apikey=thewdb') 
 	.then(response => {
 		console.log(response);
-		let movies = response.data.results;
+		let movies = response.data.Search;
 		let output = '';
 		$.each(movies, (index, movie) => {
 			output += `
@@ -28,6 +28,54 @@ function getMovies(searchText) {
 	.catch(err => {
 		console.error(err);
 	});
+}	
+
+function movieSelected(id) {
+	sessionStorage.setItem("movieId", id);
+	window.location= 'movie.html';
+	return false;
+}	
+
+function getMovie() {
+	let movieId = sessionStorage.getItem('movieId');
+
+	axios.get('http://www.omdbapi.com?i='+ movieId+'&apikey=thewdb') 
+	.then(response => {
+		console.log(response);
+		let movie = response.data;
+		let output =`
+			<div class="row">
+			<div class="col-md-4">
+				<img src="${movie.Poster}" class="thumbnail">
+			</div>
+			<div class="col-md-8">
+				<h2>${movie.Title}</h2>
+				<ul class="list-group">
+				<li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+				<li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+				<li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+				<li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+				<li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+				<li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+				<li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+			</div
+		</div
+		<div class="row'>
+			<div class="well">
+			<h3>Plot</h3>
+			${movie.Plot}
+			<hr>
+			<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+			<a href="index.html" class="btn btn-default">Go Back To Search</a>
+			
+		`;
+		$('#movie').html(output);
+	})
+	.catch(err => {
+		console.error(err);
+	});
+
+}
 
 
 
@@ -84,4 +132,4 @@ fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
 .catch(err => {
 	console.error(err);
 });
-// ==============================>*/}
+// ==============================>*/
