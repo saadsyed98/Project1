5d5667af8e2aa856303b12e88f90d813ed10b9cb
Derick Hooley <dghooley@gmail.com>
2021-09-21 23:39:12 -0500

    jqueryAPI

--- assets/watchmode.js
+++ assets/watchmode.js
@@ -7,33 +7,18 @@ $(document).ready(() => {
         }); 
     });
 
-    function getSource() {
-        fetch("https://watchmode.p.rapidapi.com/title/3173903/sources/", {
-            "method": "GET",
-            "headers": {
-                "regions": "US",
-                "x-rapidapi-host": "watchmode.p.rapidapi.com",
-                "x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
-            }
-        })
-        .then(response => {
-            console.log(response);
-            let source = response.data;
-            let output = '';
-            $.each(source, (index, source) => {
-                output += `
-                <div class = "col-md-3">
-                <div class = "well text-center">
-                <h5> ${source.name}</h5>
-                <a onclick = "movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>		
-                `;
-            });
-        
-            $('#source').html(output);
-        })
-        .catch(err => {
-            console.error(err);
-        });
-        
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://watchmode.p.rapidapi.com/title/3173903/sources/",
+        "method": "GET",
+        "headers": {
+            "regions": "US",
+            "x-rapidapi-host": "watchmode.p.rapidapi.com",
+            "x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
         }
-        
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
