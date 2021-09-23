c26003e1936f18d4a452c0064e7860f985120573
Saad Syed <saadsyed1998@gmail.com>
2021-09-23 18:06:44 -0400

    final pushed back up

--- assets/js/where2watch.js
+++ assets/js/where2watch.js
@@ -77,8 +77,6 @@ function getMovie() {
 		console.error(err);
 	});
 
-//	getSource();
-
 }
 
 
@@ -99,3 +97,41 @@ function getMovie() {
 
 
 
+/*// Utelly API =========================>
+fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt3398228&source=imdb&country=us", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+// ========================>
+
+
+// GoWATCH ========================>
+fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-host": "gowatch.p.rapidapi.com",
+		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
+	},
+	"body": {
+		"id": "tt6751668",
+		"type": "movie",
+		"country": "us"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+// ==============================>*/
