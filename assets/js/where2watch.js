0cf4b71bb26ebb0164c0da8e38c330c3f4a70081
Derick G. Hooley <dghooley@gmail.com>
2021-09-23 22:41:40 -0500

    Update where2watch.js

--- assets/js/where2watch.js
+++ assets/js/where2watch.js
@@ -13,7 +13,7 @@ function getMovies(searchText) {
 		console.log(response);
 		let movies = response.data.Search;
 		let output = '';
-		$.each(movies, (index, movie) => {
+		$.each(movies, (movieSearch, movie) => {
 			output += `
 			<div class = "col-md-3">
 			<div class = "well text-center">
@@ -68,7 +68,7 @@ function getMovie() {
 			<hr>
 			<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
 			<br>
-			<a href="movie-search.html" class="btn btn-default">Go Back To Search</a>
+			<a href="movieSearch.html" class="btn btn-default">Go Back To Search</a>
 			
 		`;
 		$('#movie').html(output);
@@ -78,60 +78,3 @@ function getMovie() {
 	});
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*// Utelly API =========================>
-fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt3398228&source=imdb&country=us", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-// ========================>
-
-
-// GoWATCH ========================>
-fetch("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", {
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"x-rapidapi-host": "gowatch.p.rapidapi.com",
-		"x-rapidapi-key": "a560eed208mshfe3e8b958f39c50p17b66cjsn4a55ced8d8c5"
-	},
-	"body": {
-		"id": "tt6751668",
-		"type": "movie",
-		"country": "us"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-// ==============================>*/
