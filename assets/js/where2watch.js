3f836df339c2ba783a7ec5fcff2313c75f805751
Derick G. Hooley <dghooley@gmail.com>
2021-09-23 22:58:38 -0500

    Update where2watch.js

--- assets/js/where2watch.js
+++ assets/js/where2watch.js
@@ -13,7 +13,7 @@ function getMovies(searchText) {
 		console.log(response);
 		let movies = response.data.Search;
 		let output = '';
-		$.each(movies, (movieSearch, movie) => {
+		$.each(movies, (index, movie) => {
 			output += `
 			<div class = "col-md-3">
 			<div class = "well text-center">
